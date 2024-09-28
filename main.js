88094ed5fff6dc1609523609ec446900cc4d787b
JohnReyGimenez <gimenezjr5@gmail.com>
2024-09-28 14:01:09 +0800

    added a function that gets the numbers from the keyswhen pressed

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('hi')
+document.addEventListener("DOMContentLoaded", function() {
+    let clear = document.querySelector(".clear");
+    let equal = document.querySelector(".equals");
+    let decimal = document.querySelector(".decimal");
+    let back = document.querySelector(".back");
+
+    let numbers = document.querySelectorAll(".number");
+    let operator = document.querySelectorAll(".operator");
+
+    let display = document.querySelector(".display");
+
+    numbers.forEach((number) => number.addEventListener("click", function(e) {
+        handleNumber(e.target.textContent)
+    })
+);
+    
+})
+
+function handleNumber(num) {
+    console.log(num);
+}
